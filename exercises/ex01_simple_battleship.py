3b3dce11d91b90a62fadf509bdff9d36a9e3c585
Luke Rodden <lrodden@unc.edu>
2024-01-19 15:59:14 -0500

    ex01 v3

--- exercises/ex01_simple_battleship.py
+++ exercises/ex01_simple_battleship.py
@@ -11,37 +11,37 @@ WHITE_BOX: str = "\U00002B1C"
 p1_choice: str = input("Pick a secret boat location between 1 and 4: ")
 p1_number: int = int(p1_choice)
 if p1_number < 1:
-    print (" Error!" + p1_choice + " too low!")
-    exit()
+   print(" Error!" + p1_choice + " too low!")
+   exit()
 if p1_number > 4:
- print (" Error!" + p1_choice + " too high!") 
- exit()
+   print(" Error!" + p1_choice + " too high!") 
+   exit()
 
 
 p2_choice: str = input("Guess a number between 1 and 4: ")
 p2_number: int = int(p2_choice)
 if p2_number < 1:
-    print (" Error!" + p2_choice + " too low!") 
-    exit()
+   print(" Error!" + p2_choice + " too low!") 
+   exit()
 if p2_number > 4:
- print (" Error!" + p2_choice + " too high!")
- exit()
+   print(" Error!" + p2_choice + " too high!")
+   exit()
 
 if p1_number == p2_number:
-   print ("Correct! You hit the ship.")
-   result: str= RED_BOX
+   print("Correct! You hit the ship.")
+   result: str = RED_BOX
 else: 
    print("Incorrect! You missed the ship.")
-   result: str= WHITE_BOX
+   result: str = WHITE_BOX
 
 if p2_number == 1:
-   print( result + BLUE_BOX + BLUE_BOX + BLUE_BOX )
+   print(result + BLUE_BOX + BLUE_BOX + BLUE_BOX)
 
 if p2_number == 2:
-   print( BLUE_BOX + result + BLUE_BOX + BLUE_BOX )
+   print(BLUE_BOX + result + BLUE_BOX + BLUE_BOX)
 
 if p2_number == 3:
-   print( BLUE_BOX + BLUE_BOX + result + BLUE_BOX )
+   print(BLUE_BOX + BLUE_BOX + result + BLUE_BOX)
 
 if p2_number == 4:
-   print( BLUE_BOX + BLUE_BOX + BLUE_BOX + result )
+   print(BLUE_BOX + BLUE_BOX + BLUE_BOX + result)
