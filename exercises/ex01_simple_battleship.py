6824d5c78da7bf430362060af56e6b5437d4af7e
Luke Rodden <lrodden@unc.edu>
2024-01-19 15:49:57 -0500

    ex1 v2

--- exercises/ex01_simple_battleship.py
+++ exercises/ex01_simple_battleship.py
@@ -8,23 +8,23 @@ RED_BOX: str = "\U0001F7E5"
 WHITE_BOX: str = "\U00002B1C"
 
 
-p1_choice: str = input("pick a secret boat location between 1 and 4")
+p1_choice: str = input("Pick a secret boat location between 1 and 4: ")
 p1_number: int = int(p1_choice)
 if p1_number < 1:
     print (" Error!" + p1_choice + " too low!")
     exit()
 if p1_number > 4:
- print (" Error!" + p1_choice + " to high!") 
+ print (" Error!" + p1_choice + " too high!") 
  exit()
 
 
-p2_choice: str = input("pick a secret boat location between 1 and 4")
+p2_choice: str = input("Guess a number between 1 and 4: ")
 p2_number: int = int(p2_choice)
 if p2_number < 1:
     print (" Error!" + p2_choice + " too low!") 
     exit()
 if p2_number > 4:
- print (" Error!" + p2_choice + " to high!")
+ print (" Error!" + p2_choice + " too high!")
  exit()
 
 if p1_number == p2_number:
